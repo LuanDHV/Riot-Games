83d12bde25168f4b8af2965a096e783ca3ebb7e4
LuanDHV <luandhv1406@gmail.com>
2024-11-21 20:43:13 +0700

    update ui tft page

--- frontend/src/app/(games)/teamfighttactics/page.tsx
+++ frontend/src/app/(games)/teamfighttactics/page.tsx
@@ -1,5 +1,7 @@
 import Banner from "./components/Banner";
 import Battle from "./components/Battle";
+import GetYourGameOne from "./components/GetYourGameOne";
+import HowToPlay from "./components/HowToPlay";
 import LatestNews from "./components/LatestNews";
 import Magic from "./components/Magic";
 import PickUpAndGo from "./components/PickUpAndGo";
@@ -14,6 +16,8 @@ export default function TeamFightTacticsPage() {
       <StyleOnEm />
       <Magic />
       <LatestNews />
+      <HowToPlay />
+      <GetYourGameOne />
     </>
   );
 }
