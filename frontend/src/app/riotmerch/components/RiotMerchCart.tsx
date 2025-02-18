42a02e6fb33f019a4a0bcc5d0aec56e3deca7cf3
LuanDHV <luandhv1406@gmail.com>
2025-02-18 11:23:33 +0700

    update ui riot merch cart

--- frontend/src/app/riotmerch/components/RiotMerchCart.tsx
+++ frontend/src/app/riotmerch/components/RiotMerchCart.tsx
@@ -5,6 +5,7 @@ export default function RiotMerchCart({
   setOpenRiotMerchCart,
 }: IRiotMerchCartProps) {
   if (!openRiotMerchCart) return null;
+
   return (
     <>
       <div className="fixed inset-0 z-50 flex justify-end">
@@ -15,7 +16,7 @@ export default function RiotMerchCart({
         ></div>
 
         {/* Cart Menu */}
-        <div className="relative right-0 flex h-full w-full bg-white md:w-2/5 xl:w-2/6">
+        <div className="relative right-0 flex h-full w-full bg-white md:w-[448px]">
           <div className="flex w-full flex-col pt-10">
             <div className="w-full px-8 pb-8">
               <div className="flex items-center justify-between">
@@ -36,9 +37,65 @@ export default function RiotMerchCart({
               </div>
             </div>
             <div className="w-full px-8 pb-8">
-              <div className=""> 2</div>
+              <div className="border-y-[1px] border-[#d0d0d0] py-8">
+                Your cart is empty
+              </div>
+
+              {/* Show When Cart is not Empty */}
+              {/* <div className="flex gap-4 border-y-[1px] border-[#d0d0d0] py-8">
+                <div className="flex min-h-[103px] min-w-[103px] items-center justify-center">
+                  <img
+                    src="/imgs/riotmerch/products/arcane/Test-Img-Add-To-Card.png"
+                    alt="Img Product"
+                    className="h-[76px] w-[76px] object-cover"
+                  />
+                </div>
+                <div className="flex w-full flex-col justify-between">
+                  <div className="text-base font-bold">Diana Unlocked</div>
+                  <div className="">$74.99</div>
+                  <div className="flex items-center justify-end">
+                    <img
+                      src="/imgs/riotmerch/products/delete-icon.svg"
+                      alt="Delete Icon"
+                      className="cursor-pointer pr-1"
+                    />
+                    <div className="flex w-5 justify-center font-bold">1</div>
+                    <img
+                      src="/imgs/riotmerch/products/add-icon.svg"
+                      alt="Add Icon"
+                      className="cursor-pointer"
+                    />
+                  </div>
+                </div>
+              </div> */}
+
+              {/* Show When Cart is not Empty */}
+              {/* <div className="border-b-[1px] border-[#d0d0d0] py-8">
+                <div className="flex flex-col gap-8">
+                  <h2 className="flex gap-1 text-base font-bold uppercase lg:text-[28px]">
+                    Summary
+                  </h2>
+                  <div className="flex flex-col gap-2">
+                    <div className="flex justify-between">
+                      <div className="">Subtotal (1 items)</div>
+                      <div className="">USD $74.99</div>
+                    </div>
+                    <div className="flex justify-between">
+                      <div className="">Shipping & Tax</div>
+                      <div className="">Calculated at next step</div>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
             </div>
           </div>
+
+          {/* Show When Cart is not Empty */}
+          {/* <div className="absolute bottom-0 w-full border-t-[1px] border-[#d0d0d0] p-8">
+            <button className="clip-button-slant h-[50px] w-full bg-[#eb0029] text-base font-bold uppercase text-white duration-300 ease-in-out">
+              Check Out
+            </button>
+          </div> */}
         </div>
       </div>
     </>
